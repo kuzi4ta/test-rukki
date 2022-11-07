fd70fbaaf4ec87e9def1fcf682b18f1c04c24adf
Измайлов Дмитрий <kuzi4ta@gmail.com>
2022-11-07 12:51:56 +0300

    Update

--- src/AbstractApplication.php
+++ src/AbstractApplication.php
@@ -1,5 +1,41 @@
 <?php
 
+use Entity\Core;
+
 abstract class AbstractApplication {
 
+    protected function _matchRoute(array $routes): array {
+        $matchRoute = [];
+        $currentUrl = str_replace('/TestApi', '', $this->request->url());
+        foreach ($routes as $route) {
+            [$method, $apiName, $apiUrl, $controller, $action] = $route;
+
+            if ($apiUrl === null) {
+                throw new \Exception('Invalid route in `routes.php` file');
+            }
+            if ($apiUrl === $currentUrl && $method === $this->request->requestMethod()) {
+                $matchRoute = $route;
+                break;
+            }
+        }
+
+        if (count($matchRoute) === 0) {
+            throw new \Exception('Not match route');
+        }
+
+        return $matchRoute;
+    }
+
+    protected function _runController(array $matchRoute, Core\Config $config): void {
+        [$method, $apiName, $apiUrl, $controller, $actionName] = $matchRoute;
+
+        $controllerInstance = new $controller($this->request, $config);
+        $methodName = "{$actionName}Action";
+        if (!method_exists($controllerInstance, $methodName)) {
+            throw new \Exception("Undefined action. Please check controller {$controller}");
+        }
+
+        $result = $controllerInstance->$methodName($this->request);
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }
 }
--- src/ApiApplication.php
+++ src/ApiApplication.php
@@ -21,38 +21,4 @@ class ApiApplication extends AbstractApplication {
         }
     }
 
-    private function _matchRoute(array $routes): array {
-        $matchRoute = [];
-        $currentUrl = str_replace('/TestApi', '', $this->request->url());
-        foreach ($routes as $route) {
-            [$method, $apiName, $apiUrl, $controller, $action] = $route;
-
-            if ($apiUrl === null) {
-                throw new \Exception('Invalid route in `routes.php` file');
-            }
-            if ($apiUrl === $currentUrl && $method === $this->request->requestMethod()) {
-                $matchRoute = $route;
-                break;
-            }
-        }
-
-        if (count($matchRoute) === 0) {
-            throw new \Exception('Not match route');
-        }
-
-        return $matchRoute;
-    }
-
-    private function _runController(array $matchRoute, Core\Config $config): void {
-        [$method, $apiName, $apiUrl, $controller, $actionName] = $matchRoute;
-
-        $controllerInstance = new $controller($this->request, $config);
-        $methodName = "{$actionName}Action";
-        if (!method_exists($controllerInstance, $methodName)) {
-            throw new \Exception("Undefined action. Please check controller {$controller}");
-        }
-
-        $result = $controllerInstance->$methodName($this->request);
-        echo json_encode($result, JSON_PRETTY_PRINT);
-    }
 }
